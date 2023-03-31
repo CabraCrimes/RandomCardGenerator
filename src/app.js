0db1671be21eb22b1ba0da386c3e1068001cd46b
CabraCrimes <benalexanderthegreat@gmail.com>
2023-03-31 15:21:56 +0000

    Random Card Generator v2 working

--- src/app.js
+++ src/app.js
@@ -7,24 +7,31 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   let suit = ["♥", "♠", "♣", "♦"];
-  let royal = ["A", "J", "Q", "K", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  let num = ["A", "J", "Q", "K", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+  //Variables for random array elements.
   let suitStyle;
-
+  let numStyle;
+  //DOM selectors
   let cardSeed = document.querySelectorAll("#card-seed");
+  let cardParentSeed = document.querySelectorAll(".cardClass");
+
   let randomNumber = Math.floor(Math.random() * 3);
   let cardNumber = Math.floor(Math.random() * 10) * +1;
 
-  //suitStyle = suit[randomNumber]; suitStyle.style.fontSize = "x-large";
+  suitStyle = suit[randomNumber];
+  numStyle = num[cardNumber];
+  //Generates new HTML for hearts
+  if (suit[0] == suitStyle) {
+    cardParentSeed[0].innerHTML = `
+    <div id="hearts" class="col d-flex justify-content-start ">${suitStyle}</div>
+    <div class="col d-flex justify-content-center align-items-center">${numStyle}</div>
+    <div id="hearts" class="col d-flex justify-content-end align-items-end ">${suitStyle}</div>
+    `;
+  } else {
+    cardSeed[0].innerHTML = suitStyle;
+    cardSeed[1].innerHTML = numStyle;
+    cardSeed[2].innerHTML = suitStyle;
+  }
 
-  cardSeed[0].innerHTML = suit[randomNumber];
-  cardSeed[1].innerHTML = royal[cardNumber];
-  cardSeed[2].innerHTML = suit[randomNumber];
-  cardSeed.style.fontSize = "25px";
-
-  console.log(randomNumber);
+  console.log(suitStyle);
 };
-// var span = document.getElementById("span");
-// console.log(span);
-
-// span.style.fontSize = "25px";
-// span.innerHTML = "String";
